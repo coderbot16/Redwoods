81f2107435b204529664fdee479ba1037bd4f30b
coderbot <coderbot16@gmail.com>
2019-02-09 20:20:53 -0800

    Add a fast, custom leaf decay algorithm to fix issues with trees decaying away

--- src/main/java/net/coderbot/redwoods/block/BlockConiferLeaves.java
+++ src/main/java/net/coderbot/redwoods/block/BlockConiferLeaves.java
@@ -25,6 +25,7 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 import javax.annotation.Nullable;
+import java.util.BitSet;
 import java.util.Random;
 
 public class BlockConiferLeaves extends BlockLeaves {
@@ -64,6 +65,24 @@ public class BlockConiferLeaves extends BlockLeaves {
 		}
 
 		return i;
+	}
+
+	@Override
+	public void updateTick(World world, BlockPos pos, IBlockState state, Random rand) {
+		if(world.isRemote) {
+			return;
+		}
+
+		if(!state.getValue(CHECK_DECAY) || !state.getValue(DECAYABLE)) {
+			return;
+		}
+
+		if(DecayAlgorithm.canStay(world, pos)) {
+			world.setBlockState(pos, world.getBlockState(pos).withProperty(CHECK_DECAY, false));
+		} else {
+			this.dropBlockAsItem(world, pos, world.getBlockState(pos), 0);
+			world.setBlockToAir(pos);
+		}
 	}
 
 	@Override
@@ -124,4 +143,142 @@ public class BlockConiferLeaves extends BlockLeaves {
 		return true;
 	}
 
+	/**
+	 * Meant to mimic the behavior of vanilla leaf decay in {@link BlockLeaves#updateTick(World, BlockPos, IBlockState, Random)}
+	 * However, the algorithm has been revamped to be far more performant at its scale, going from O(radius^4) to O(radius^3)
+	 * In other words, decay checks for conifer leaves won't kill your TPS as much as it would with the naive vanilla algorithm.
+	 *
+	 * Performance data: Even with several entire tree's worth of leaves decaying each second, TPS only dropped to 19.5.
+	 */
+	private static class DecayAlgorithm {
+		private static final int FIELD_SIZE = 32;
+		private static final int CENTER_OFFSET = FIELD_SIZE / 2;
+		private static final byte MARKER_LEAVES = -1;
+		private static final byte MARKER_IGNORE = -2;
+		private static final int SCAN_RADIUS = 8;
+		private static final int ITERATIONS = 13;
+
+		private static final byte[] CELLS = new byte[FIELD_SIZE * FIELD_SIZE * FIELD_SIZE];
+		private static BitSet fillingQueue = new BitSet(CELLS.length);
+		private static BitSet drainingQueue = new BitSet(CELLS.length);
+
+		private static int at(int dX, int dY, int dZ) {
+			int aX = dX + CENTER_OFFSET;
+			int aY = dY + CENTER_OFFSET;
+			int aZ = dZ + CENTER_OFFSET;
+
+			return aY * FIELD_SIZE * FIELD_SIZE + aZ * FIELD_SIZE + aX;
+		}
+
+		private static int atX(int index) {
+			return (index % FIELD_SIZE) - CENTER_OFFSET;
+		}
+
+		private static int atZ(int index) {
+			return ((index / FIELD_SIZE) % FIELD_SIZE) - CENTER_OFFSET;
+		}
+
+		private static int atY(int index) {
+			return ((index / FIELD_SIZE) / FIELD_SIZE) - CENTER_OFFSET;
+		}
+
+		private static void markNeighbors(int dX, int dY, int dZ, BitSet queue) {
+			queue.set(at(dX + 1, dY, dZ));
+			queue.set(at(dX - 1, dY, dZ));
+			queue.set(at(dX, dY + 1, dZ));
+			queue.set(at(dX, dY - 1, dZ));
+			queue.set(at(dX, dY, dZ + 1));
+			queue.set(at(dX, dY, dZ - 1));
+		}
+
+		private static byte maximumNeighbor(int dX, int dY, int dZ) {
+			return (byte)Math.max(
+					Math.max(
+							Math.max(
+									CELLS[at(dX + 1, dY, dZ)],
+									CELLS[at(dX - 1, dY, dZ)]
+							),
+							Math.max(
+									CELLS[at(dX, dY + 1, dZ)],
+									CELLS[at(dX, dY - 1, dZ)]
+							)
+					),
+					Math.max(
+							CELLS[at(dX, dY, dZ + 1)],
+							CELLS[at(dX, dY, dZ - 1)]
+					)
+			);
+		}
+
+		static boolean canStay(World world, BlockPos origin) {
+			// Make sure that we don't inadvertently load chunks!
+			if (!world.isAreaLoaded(origin, SCAN_RADIUS + 2)) {
+				return true;
+			}
+
+			BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
+
+			fillingQueue.clear();
+			drainingQueue.clear();
+
+			// For each block in the possible radius:
+			for (int dY = -SCAN_RADIUS; dY <= SCAN_RADIUS; ++dY) {
+				for (int dZ = -SCAN_RADIUS; dZ <= SCAN_RADIUS; ++dZ) {
+					for (int dX = -SCAN_RADIUS; dX <= SCAN_RADIUS; ++dX) {
+						pos.setPos(origin.getX() + dX, origin.getY() + dY, origin.getZ() + dZ);
+
+						IBlockState state = world.getBlockState(pos);
+						Block block = state.getBlock();
+
+						byte light;
+
+						// Check if the block is leaves or can sustain leaves. Otherwise, disregard it.
+						if (block.canSustainLeaves(state, world, pos)) {
+							light = ITERATIONS;
+							markNeighbors(dX, dY, dZ, fillingQueue);
+						} else if(block.isLeaves(state, world, pos)) {
+							light = MARKER_LEAVES;
+						} else {
+							light = MARKER_IGNORE;
+						}
+
+						CELLS[at(dX, dY, dZ)] = light;
+					}
+				}
+			}
+
+			// Propagate the distance values just like in a lighting algorithm.
+			for (byte distance = 1; distance <= ITERATIONS; ++distance) {
+				{
+					// Swap the queues.
+					BitSet temp = drainingQueue;
+					drainingQueue = fillingQueue;
+					fillingQueue = temp;
+				}
+
+				int drainIndex = 0;
+				while(!drainingQueue.isEmpty()) {
+					drainIndex = drainingQueue.nextSetBit(drainIndex);
+					drainingQueue.clear(drainIndex);
+
+					if (CELLS[drainIndex] != MARKER_LEAVES) {
+						continue;
+					}
+
+					int dX = atX(drainIndex);
+					int dY = atY(drainIndex);
+					int dZ = atZ(drainIndex);
+
+					int potentialValue = maximumNeighbor(dX, dY, dZ) - 1;
+					if (potentialValue >= 0) {
+						CELLS[drainIndex] = (byte) potentialValue;
+						markNeighbors(dX, dY, dZ, fillingQueue);
+					}
+				}
+			}
+
+			// Finally: check if we got sustained
+			return CELLS[at(0, 0, 0)] >= 0;
+		}
+	}
 }
