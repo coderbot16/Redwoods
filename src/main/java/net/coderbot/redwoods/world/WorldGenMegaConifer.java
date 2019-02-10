8daa11be816bd9e94bf421a0e1f22f04e6e9b17d
coderbot <coderbot16@gmail.com>
2019-02-10 15:09:01 -0800

    Fix Mega Conifers generating partially floating

--- src/main/java/net/coderbot/redwoods/world/WorldGenMegaConifer.java
+++ src/main/java/net/coderbot/redwoods/world/WorldGenMegaConifer.java
@@ -67,8 +67,8 @@ public class WorldGenMegaConifer extends WorldGenAbstractTree {
 			return false;
 		}
 
-		for(int dZ = 0; dZ < 1; dZ++) {
-			for(int dX = 0; dX < 1; dX++) {
+		for(int dZ = 0; dZ < 2; dZ++) {
+			for(int dX = 0; dX < 2; dX++) {
 				BlockPos below = origin.add(dX, -1, dZ);
 
 				setBlockAndNotifyAdequately(world, below, Blocks.DIRT.getDefaultState());
