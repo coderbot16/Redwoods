5157a3c4ea1a2be07dad7da9b2c272090a247984
coderbot <coderbot16@gmail.com>
2019-02-10 16:29:33 -0800

    Add and register all 4 biomes

--- src/main/java/net/coderbot/redwoods/biomes/BiomeConiferous.java
+++ src/main/java/net/coderbot/redwoods/biomes/BiomeConiferous.java
@@ -1,28 +1,37 @@
 package net.coderbot.redwoods.biomes;
 
+import mcp.MethodsReturnNonnullByDefault;
 import net.coderbot.redwoods.block.BlockConiferSapling;
 import net.coderbot.redwoods.init.ModBlocks;
 import net.coderbot.redwoods.world.WorldGenConifer;
 import net.coderbot.redwoods.world.WorldGenMegaConifer;
+import net.minecraft.block.BlockTallGrass;
 import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+import net.minecraft.world.gen.feature.WorldGenTallGrass;
+import net.minecraft.world.gen.feature.WorldGenTrees;
+import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.common.BiomeDictionary;
 
 import java.util.Random;
 
+@MethodsReturnNonnullByDefault
 public class BiomeConiferous extends Biome {
-	WorldGenAbstractTree[] trees;
+	private WorldGenAbstractTree[] trees;
+	private int fernPercentage;
 
 	public BiomeConiferous(Properties properties) {
 		super(properties.biomeProperties);
 
-		decorator.treesPerChunk = 7;
-		decorator.flowersPerChunk = 0;
-		decorator.grassPerChunk = 16; // TODO: Use ferns
+		decorator.treesPerChunk = properties.treesPerChunk;
+		decorator.flowersPerChunk = properties.flowerCount;
+		decorator.grassPerChunk = properties.grassCount;
 		decorator.mushroomsPerChunk = 2;
 
 		spawnableCreatureList.add(new SpawnListEntry(EntityWolf.class, 5, 4, 4));
+
+		fernPercentage = properties.fernPercentage;
 
 		trees = properties.getTrees();
 		if(trees.length == 0) {
@@ -30,14 +39,26 @@ public class BiomeConiferous extends Biome {
 		}
 	}
 
+	@Override
 	public WorldGenAbstractTree getRandomTreeFeature(Random rand) {
 		if(trees.length == 0) {
-			return null;
+			return new WorldGenTrees(false);
 		} else if(trees.length == 1) {
 			return trees[0];
 		}
 
 		return trees[rand.nextInt(trees.length)];
+	}
+
+	@Override
+	public WorldGenerator getRandomWorldGenForGrass(Random rand) {
+		int percent = rand.nextInt(100); // 0-99
+
+		if(percent < fernPercentage) {
+			return new WorldGenTallGrass(BlockTallGrass.EnumType.FERN);
+		} else {
+			return new WorldGenTallGrass(BlockTallGrass.EnumType.GRASS);
+		}
 	}
 
 	public static class Properties {
@@ -46,6 +67,10 @@ public class BiomeConiferous extends Biome {
 		public boolean redwoodMega;
 		public boolean firNormal;
 		public boolean firMega;
+		public int fernPercentage = 0;
+		public int grassCount = 0;
+		public int flowerCount = 0;
+		public int treesPerChunk = 7;
 
 		public Properties(String name, BiomeDictionary.Type... types) {
 			biomeProperties = new BiomeProperties(name);
--- src/main/java/net/coderbot/redwoods/init/ModBiomes.java
+++ src/main/java/net/coderbot/redwoods/init/ModBiomes.java
@@ -4,6 +4,7 @@ import net.coderbot.redwoods.Redwoods;
 import net.coderbot.redwoods.biomes.BiomeConiferous;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
+import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -14,19 +15,19 @@ public class ModBiomes {
 
 	// Mega Redwoods
 	public static BiomeConiferous REDWOOD_FOREST;
-	private static BiomeConiferous.Properties REDWOOD_FOREST_PROPS;
+	public static BiomeConiferous.Properties REDWOOD_FOREST_PROPS;
 
 	// Normal Firs + Mega Redwoods
 	public static BiomeConiferous LUSH_REDWOOD_FOREST;
-	private static BiomeConiferous.Properties LUSH_REDWOOD_FOREST_PROPS;
+	public static BiomeConiferous.Properties LUSH_REDWOOD_FOREST_PROPS;
 
 	// Normal Firs + Mega Firs
 	public static BiomeConiferous TEMPERATE_RAINFOREST;
-	private static BiomeConiferous.Properties TEMPERATE_RAINFOREST_PROPS;
+	public static BiomeConiferous.Properties TEMPERATE_RAINFOREST_PROPS;
 
 	// Normal Firs + Mega Firs + Snow
 	public static BiomeConiferous SNOWY_RAINFOREST;
-	private static BiomeConiferous.Properties SNOWY_RAINFOREST_PROPS;
+	public static BiomeConiferous.Properties SNOWY_RAINFOREST_PROPS;
 
 	@SubscribeEvent
 	public static void registerBiomes(RegistryEvent.Register<Biome> event) {
@@ -38,6 +39,8 @@ public class ModBiomes {
 				.setBaseHeight(1.2F)
 				.setHeightVariation(0.3F);
 		REDWOOD_FOREST_PROPS.redwoodMega = true;
+		REDWOOD_FOREST_PROPS.fernPercentage = 100;
+		REDWOOD_FOREST_PROPS.grassCount = 1;
 		REDWOOD_FOREST = registerConiferBiome(event, "redwood_forest", REDWOOD_FOREST_PROPS);
 		BiomeDictionary.addTypes(REDWOOD_FOREST,
 				BiomeDictionary.Type.HOT,
@@ -48,6 +51,7 @@ public class ModBiomes {
 				BiomeDictionary.Type.HILLS,
 				BiomeDictionary.Type.LUSH
 		);
+		BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(REDWOOD_FOREST, 8));
 
 		// Lush Redwood Forest
 		LUSH_REDWOOD_FOREST_PROPS = new BiomeConiferous.Properties("Lush Redwood Forest");
@@ -58,6 +62,10 @@ public class ModBiomes {
 				.setHeightVariation(0.3F);
 		LUSH_REDWOOD_FOREST_PROPS.redwoodMega = true;
 		LUSH_REDWOOD_FOREST_PROPS.firNormal = true;
+		LUSH_REDWOOD_FOREST_PROPS.fernPercentage = 100;
+		LUSH_REDWOOD_FOREST_PROPS.grassCount = 1;
+		LUSH_REDWOOD_FOREST_PROPS.flowerCount = 1;
+		LUSH_REDWOOD_FOREST_PROPS.treesPerChunk = 8;
 		LUSH_REDWOOD_FOREST = registerConiferBiome(event, "lush_redwood_forest", LUSH_REDWOOD_FOREST_PROPS);
 		BiomeDictionary.addTypes(LUSH_REDWOOD_FOREST,
 				BiomeDictionary.Type.HOT,
@@ -68,6 +76,7 @@ public class ModBiomes {
 				BiomeDictionary.Type.HILLS,
 				BiomeDictionary.Type.LUSH
 		);
+		BiomeManager.addBiome(BiomeManager.BiomeType.WARM, new BiomeManager.BiomeEntry(LUSH_REDWOOD_FOREST, 8));
 
 		// Temperate Rainforest
 		TEMPERATE_RAINFOREST_PROPS = new BiomeConiferous.Properties("Temperate Rainforest");
@@ -76,8 +85,11 @@ public class ModBiomes {
 				.setRainfall(0.9F)
 				.setBaseHeight(0.95F)
 				.setHeightVariation(0.55F);
-		TEMPERATE_RAINFOREST_PROPS.redwoodMega = true;
+		TEMPERATE_RAINFOREST_PROPS.firMega = true;
 		TEMPERATE_RAINFOREST_PROPS.firNormal = true;
+		TEMPERATE_RAINFOREST_PROPS.fernPercentage = 75;
+		TEMPERATE_RAINFOREST_PROPS.grassCount = 16;
+		TEMPERATE_RAINFOREST_PROPS.treesPerChunk = 17;
 		TEMPERATE_RAINFOREST = registerConiferBiome(event, "temperate_rainforest", TEMPERATE_RAINFOREST_PROPS);
 		BiomeDictionary.addTypes(TEMPERATE_RAINFOREST,
 				BiomeDictionary.Type.DENSE,
@@ -87,6 +99,7 @@ public class ModBiomes {
 				BiomeDictionary.Type.HILLS,
 				BiomeDictionary.Type.LUSH
 		);
+		BiomeManager.addBiome(BiomeManager.BiomeType.COOL, new BiomeManager.BiomeEntry(TEMPERATE_RAINFOREST, 10));
 
 		// Snowy Rainforest
 		SNOWY_RAINFOREST_PROPS = new BiomeConiferous.Properties("Snowy Rainforest");
@@ -96,8 +109,11 @@ public class ModBiomes {
 				.setBaseHeight(0.95F)
 				.setHeightVariation(0.55F)
 				.setSnowEnabled();
-		SNOWY_RAINFOREST_PROPS.redwoodMega = true;
+		SNOWY_RAINFOREST_PROPS.firMega = true;
 		SNOWY_RAINFOREST_PROPS.firNormal = true;
+		SNOWY_RAINFOREST_PROPS.fernPercentage = 75;
+		SNOWY_RAINFOREST_PROPS.grassCount = 16;
+		SNOWY_RAINFOREST_PROPS.treesPerChunk = 17;
 		SNOWY_RAINFOREST = registerConiferBiome(event, "snowy_rainforest", SNOWY_RAINFOREST_PROPS);
 		BiomeDictionary.addTypes(SNOWY_RAINFOREST,
 				BiomeDictionary.Type.DENSE,
@@ -107,6 +123,7 @@ public class ModBiomes {
 				BiomeDictionary.Type.HILLS,
 				BiomeDictionary.Type.LUSH
 		);
+		BiomeManager.addBiome(BiomeManager.BiomeType.ICY, new BiomeManager.BiomeEntry(SNOWY_RAINFOREST, 10));
 	}
 
 	private static BiomeConiferous registerConiferBiome(RegistryEvent.Register<Biome> event, String name, BiomeConiferous.Properties properties) {
