764396707de77ef0f12efbd5823afa7f92ad9375
coderbot <coderbot16@gmail.com>
2019-02-14 20:33:20 -0800

    Add logs to smelting recipes

--- src/main/java/net/coderbot/redwoods/Redwoods.java
+++ src/main/java/net/coderbot/redwoods/Redwoods.java
@@ -1,14 +1,17 @@
 package net.coderbot.redwoods;
 
+import net.coderbot.redwoods.init.ModBlocks;
 import net.coderbot.redwoods.init.ModItems;
 import net.coderbot.redwoods.proxy.CommonProxy;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 import javax.annotation.Nonnull;
 
@@ -43,5 +46,9 @@ public class Redwoods
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event) {
 		ModItems.registerOreDict();
+		GameRegistry.addSmelting(ModBlocks.REDWOOD_LOG, new ItemStack(Items.COAL, 1, 1), 0.15F);
+		GameRegistry.addSmelting(ModBlocks.REDWOOD_LOG_QUARTER, new ItemStack(Items.COAL, 1, 1), 0.15F);
+		GameRegistry.addSmelting(ModBlocks.FIR_LOG, new ItemStack(Items.COAL, 1, 1), 0.15F);
+		GameRegistry.addSmelting(ModBlocks.FIR_LOG_QUARTER, new ItemStack(Items.COAL, 1, 1), 0.15F);
 	}
 }
