c02a3fe3ec813952fc7a1ecae0c0d3d55fd9178f
coderbot <coderbot16@gmail.com>
2019-02-17 12:07:21 -0800

    Really Release 1.1.0

--- src/main/java/net/coderbot/redwoods/Redwoods.java
+++ src/main/java/net/coderbot/redwoods/Redwoods.java
@@ -20,7 +20,7 @@ public class Redwoods
 {
 	public static final String MODID = "redwoods";
 	public static final String NAME = "Redwoods";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.0";
 
 	@SidedProxy(
 			serverSide = "net.coderbot.redwoods.proxy.CommonProxy",
