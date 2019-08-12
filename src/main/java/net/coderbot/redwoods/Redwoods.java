cae0b14c0bc0d865b206780aca93c496b4277f4c
coderbot <coderbot16@gmail.com>
2019-08-11 17:13:38 -0700

    Redwoods 1.2.2

--- src/main/java/net/coderbot/redwoods/Redwoods.java
+++ src/main/java/net/coderbot/redwoods/Redwoods.java
@@ -20,7 +20,7 @@ public class Redwoods
 {
 	public static final String MODID = "redwoods";
 	public static final String NAME = "Redwoods";
-	public static final String VERSION = "1.2.1";
+	public static final String VERSION = "1.2.2";
 
 	@SidedProxy(
 			serverSide = "net.coderbot.redwoods.proxy.CommonProxy",
